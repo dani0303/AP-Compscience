883982c47ae55d5f4c3b10561e5e3c977595155b
rodger2003 <48264113+dani0303@users.noreply.github.com>
2021-10-26 13:33:40 -0400

    Create CylinderTest.java

---
+++ Chapter4/Question8/CylinderTest.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class CylinderTest here.
+ *
+ * @author (Daniel Rodriguez)
+ * @version (Question8b 10/26/21)
+ */
+
+import java.util.Scanner;
+
+public class CylinderTest
+{   
+    public static void main(String[] args)
+    {   
+        Scanner s1 = new Scanner(System.in);
+        
+        System.out.println("Enter the radius ");
+        int radius = s1.nextLine();
+        
+        
+    }
+}
